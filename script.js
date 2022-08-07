2b03d8c77964648bd3df47638ca8bb2358c9c3ef
Sebastian <ssansone124@gmail.com>
2022-08-07 14:50:23 -0400

    Update with alphabet grid display

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-const wordsFive= 'about above abuse actor acute admit adopt adult after again agent agree ahead alarm album alert alike alive allow alone along alter among anger Angle angry apart apple apply arena argue arise array aside asset audio audit avoid award aware badly baker bases basic basis beach began begin begun being below bench billy birth black blame blind block blood board boost booth bound brain brand bread break breed brief bring broad broke brown build built buyer cable calif carry catch cause chain chair chart chase cheap check chest chief child china chose civil claim class clean clear click clock close coach coast could count court cover craft crash cream crime cross crowd crown curve cycle daily dance dated dealt death debut delay depth doing doubt dozen draft drama drawn dream dress drill drink drive drove dying eager early earth eight elite empty enemy enjoy enter entry equal error event every exact exist extra faith false fault fiber field fifth fifty fight final first fixed flash fleet floor fluid focus force forth forty forum found frame frank fraud fresh front fruit fully funny giant given glass globe going grace grade grand grant grass great green gross group grown guard guess guest guide happy harry heart heavy hence henry horse hotel house human ideal image index inner input issue japan jimmy joint jones judge known label large laser later laugh layer learn lease least leave legal level lewis light limit links lives local logic loose lower lucky lunch lying magic major maker march maria match maybe mayor meant media metal might minor minus mixed model money month moral motor mount mouse mouth movie music needs never newly night noise north noted novel nurse occur ocean offer often order other ought paint panel paper party peace peter phase phone photo piece pilot pitch place plain plane plant plate point pound power press price pride prime print prior prize proof proud prove queen quick quiet quite radio raise range rapid ratio reach ready refer right rival river robin roger roman rough round route royal rural scale scene scope score sense serve seven shall shape share sharp sheet shelf shell shift shirt shock shoot short shown sight since sixth sixty sized skill sleep slide small smart smile smith smoke solid solve sorry sound south space spare speak speed spend spent split spoke sport staff stage stake stand start state steam steel stick still stock stone stood store storm story strip stuck study stuff style sugar suite super sweet table taken taste taxes teach teeth terry texas thank theft their theme there these thick thing think third those three threw throw tight times tired title today topic total touch tough tower track trade train treat trend trial tried tries truck truly trust truth twice under undue union unity until upper upset urban usage usual valid value video virus visit vital voice waste watch water wheel where which while white whole whose woman women world worry worse worst worth would wound write wrong wrote yield young youth';
-const acceptedWords=[...wordsFive];
+/*
+    notes 8-7-22 @ 2:30am
+    while attempting to do some light changes I noticed
+    that the game stops marking things correct after the first letter
+    the keyboard is repeating itself and I don't know why
+*/
+
+const wordsFive='about above abuse actor acute admit adopt adult after again agent agree ahead alarm album alert alike alive allow alone along alter among anger Angle angry apart apple apply arena argue arise array aside asset audio audit avoid award aware badly baker bases basic basis beach began begin begun being below bench billy birth black blame blind block blood board boost booth bound brain brand bread break breed brief bring broad broke brown build built buyer cable calif carry catch cause chain chair chart chase cheap check chest chief child china chose civil claim class clean clear click clock close coach coast could count court cover craft crash cream crime cross crowd crown curve cycle daily dance dated dealt death debut delay depth doing doubt dozen draft drama drawn dream dress drill drink drive drove dying eager early earth eight elite empty enemy enjoy enter entry equal error event every exact exist extra faith false fault fiber field fifth fifty fight final first fixed flash fleet floor fluid focus force forth forty forum found frame frank fraud fresh front fruit fully funny giant given glass globe going grace grade grand grant grass great green gross group grown guard guess guest guide happy harry heart heavy hence henry horse hotel house human ideal image index inner input issue japan jimmy joint jones judge known label large laser later laugh layer learn lease least leave legal level lewis light limit links lives local logic loose lower lucky lunch lying magic major maker march maria match maybe mayor meant media metal might minor minus mixed model money month moral motor mount mouse mouth movie music needs never newly night noise north noted novel nurse occur ocean offer often order other ought paint panel paper party peace peter phase phone photo piece pilot pitch place plain plane plant plate point pound power press price pride prime print prior prize proof proud prove queen quick quiet quite radio raise range rapid ratio reach ready refer right rival river robin roger roman rough round route royal rural scale scene scope score sense serve seven shall shape share sharp sheet shelf shell shift shirt shock shoot short shown sight since sixth sixty sized skill sleep slide small smart smile smith smoke solid solve sorry sound south space spare speak speed spend spent split spoke sport staff stage stake stand start state steam steel stick still stock stone stood store storm story strip stuck study stuff style sugar suite super sweet table taken taste taxes teach teeth terry texas thank theft their theme there these thick thing think third those three threw throw tight times tired title today topic total touch tough tower track trade train treat trend trial tried tries truck truly trust truth twice under undue union unity until upper upset urban usage usual valid value video virus visit vital voice waste watch water wheel where which while white whole whose woman women world worry worse worst worth would wound write wrong wrote yield young youth';
+
 //save these for later!
 //`helpme please ohgod theyare watching usall`;
-//
-//
-//
-//
+
 const dictFive=wordsFive.toUpperCase().split(' ');
+const acceptedWords=[...dictFive];
+//console.log(acceptedWords);
 const wordGrid=document.getElementById('words');
 const guessGrid=document.getElementById('guess');
 const letterCell=document.createElement('div');
@@ -21,10 +27,10 @@ const msgBox=document.querySelector('.msgBox');
 const h4=document.createElement('h4');
 const firstInput=document.getElementById(`input0`);
 
-let startingWord=`${dictFive[Math.floor(Math.random()*(dictFive.length-1))]}`;
+let startingWord='';
 let lastWord='';
-let wordLength=startingWord.length;
-let letters=startingWord.split('');
+let wordLength;
+let letters=[];
 let tempList=[...letters];
 let guessList=[];
 let correctList=[];
@@ -32,6 +38,8 @@ let numofGuesses=0;
 let rowCounter=1;
 let myRow=1;
 let myCell=0;
+//console.log(letters);
+//console.log(startingWord);
 
 /*
 future ideas.. 
@@ -46,6 +54,82 @@ have a box where they can enter the clue and big msg if they get it right
 
 //console.log(startingWord);
 //console.log(letters)
+let topRow='q w e r t y u i o p'; 
+let midRow='a s d f g h j k l'; 
+let botRow='z x c v b n m';
+const topAlphaList=topRow.toUpperCase().split(" ");
+const midAlphaList=midRow.toUpperCase().split(" ");
+const botAlphaList=botRow.toUpperCase().split(" ");
+const alphaList= topAlphaList.concat(midAlphaList,botAlphaList);
+//console.log(alphaList)
+
+function alphabetGrid(){
+    const alphaBox=document.querySelector('.alphaBox');
+    const topAlphaBox=document.querySelector('.topRow');
+    const midAlphaBox=document.querySelector('.midRow');
+    const botAlphaBox=document.querySelector('.botRow');
+    
+    topAlphaList.forEach(a => {
+        const alphabet=document.createElement('div');
+        alphabet.classList.add('alphabet',`${a}`);
+        alphabet.textContent=a;
+        topAlphaBox.appendChild(alphabet);
+        });
+
+    midAlphaList.forEach(a => {
+        const alphabet=document.createElement('div');
+        alphabet.classList.add('alphabet',`${a}`);
+        alphabet.textContent=a;
+        midAlphaBox.appendChild(alphabet);
+        });
+
+    botAlphaList.forEach(a => {
+        const alphabet=document.createElement('div');
+        alphabet.classList.add('alphabet',`${a}`);
+        alphabet.textContent=a;
+        botAlphaBox.appendChild(alphabet);
+    });
+};
+
+function keyboardColorChange(){
+    const currentLetters=document.querySelectorAll('.currentRow');
+    const getKeyboard=document.querySelectorAll('.alphabet');
+    let tempList=[];
+    let index=0;
+    let className='';
+    let classArray=[];
+    //array.from is not working for me here
+    for(let i=0;i<currentLetters.length;i++){
+        tempList[i]=currentLetters[i].textContent;
+    }
+    console.log(tempList);
+
+    currentLetters.forEach(letter => {
+    
+    let text=letter.textContent;
+    className = currentLetters[index].classList.value;
+    classArray=className.split(" ");
+
+    //console.log(text);
+    //console.log(className);
+    
+    //cycle through each currentLetter ex. 'a' 26 times
+    //if currentLetter = alphabet letter
+    //get the class of the currentLetter and add it to the alphabet letter
+    //alphabet classes only add one way wrong>almost>correct
+    //classes can't go backwards in value
+
+    for(let i=0;i<alphaList.length;i++){
+        const keyClass = document.querySelector(`.${text}`).classList.value;
+        if(alphaList[i]===text){
+            if(!keyClass.includes('correct')){
+                document.querySelector(`.${text}`).classList.add(`${classArray[2]}`);
+            }
+        };
+    };
+ index++
+});
+};
 
 function createRow(){
     //console.log(wordLength);
@@ -104,24 +188,29 @@ function submitGuess(){
             guessList.push(text);   
     });
 
-    let guessString = guessList.toString();
+    let guessString = guessList.join("");
+    //console.log(guessList);
+
     const h5=document.createElement('h5');
     if(acceptedWords.includes(guessString)){
         //console.log(guessList);
         checkGuess();
         checkAlmost();
+        setRowValues();
+        keyboardColorChange();
         assignColors();
         numofGuesses++;
         guessNum--;
         rowCounter++;
         remGuesses();
-        setRowValues();
         clearGuess();
         gameOver();
         changeRowClass();
         h5.textContent='';
+        msgBox.appendChild(h5);
     } else {
         h5.textContent='';
+        msgBox.appendChild(h5);
         h5.textContent='Please enter a better word';
         msgBox.appendChild(h5);
         clearGuess();
@@ -129,22 +218,25 @@ function submitGuess(){
 };
 
 function checkGuess(){
-
     const letter=document.querySelectorAll(`.currentRow`);
     let index=0;
+    letters=startingWord.split('');
     tempList=[...letters];
     //console.log(tempList);
 
-    guessList.forEach(item =>{
-            if(guessList[index]===letters[index]){
+    guessList.forEach(() =>{
+
+            if(guessList[index]===tempList[index]){
                 letter[index].classList.add('correct');
+                //console.log(letter[index]);
                 tempList[index]='';
-                tempList.filter(word => word.length>0);
             } else {
-                letter[index].classList.add('incorrect');
+                letter[index].classList.add('wrong');
+                //console.log(letter[index]);
             };
             index++;
-        });
+        });            
+        tempList.filter(word => word.length>0);
         //console.log(guessList);
         //console.log(tempList);
 };
@@ -152,21 +244,21 @@ function checkGuess(){
 function checkAlmost(){
     const letter=document.querySelectorAll(`.currentRow`);
     let index=0;
-    //let tempList=[...letters];
     //console.log(tempList);
 
     guessList.forEach(item =>{
-        if(tempList.includes(guessList[index])){
-             letter[index].classList.remove('incorrect');
+        if(tempList.includes(item)){
+             letter[index].classList.remove('wrong');
              letter[index].classList.add('almost');
         };
+    index++;
     });
 };
 
 function assignColors(){
     document.querySelectorAll('.correct').forEach((x)=>x.style.background='green');
     document.querySelectorAll('.almost').forEach((x)=>x.style.background='yellow')
-    document.querySelectorAll('.incorrect').forEach((x)=>x.style.background='red')
+    document.querySelectorAll('.wrong').forEach((x)=>x.style.background='red')
 };
 
 function setRowValues(){
@@ -228,8 +320,8 @@ function startUp(){
     h4.textContent='';
     guessList=[];
     numofGuesses=0;
-    rowCounter=0;
-    myRow=0;
+    rowCounter=1;
+    myRow=1;
     myCell=0;
     getNewWord();
     createRow();    
@@ -242,12 +334,19 @@ function startUp(){
     firstInput.focus();
     firstInput.select();
     console.log(startingWord);
+    //console.log(letters);
 }
 
 function winMessage(){
-    h4.textContent=
-    `You won in ${guessNum} guesses! 
+    if(numofGuesses==1){
+        h4.textContent=
+    `WOW. You won in only ${numofGuesses} guess! 
     Select 'New Game' if you'd like to play again.`;
+    } else{ 
+    h4.textContent=
+    `You won in ${numofGuesses} guesses! 
+    Select 'New Game' if you'd like to play again.`;
+    };
     msgBox.appendChild(h4);
 }
 
@@ -278,6 +377,7 @@ function autoSelectInput(length){
 */
 
 document.onload=startUp();
+alphabetGrid();
 submitBtn.addEventListener('click',submitGuess);
 resetBtn.addEventListener('click',startUp);
 
